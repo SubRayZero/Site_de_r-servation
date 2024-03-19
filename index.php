ecff88e60777fbb3ebd29dae94d322c637e5fb47
SubRayZero <jasmine.draperi.simplon@gmail.com>
2024-03-19 13:49:48 +0100

    accueil css finish

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Accueil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="Components/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+
+<body>
+
+    <?php
+    require 'Components/header.php';
+    ?>
+
+    <section class="section_red">
+
+        <div class="container_texte">
+            <div class="texte">Réservez vos prochaines armes</div>
+        </div>
+        <div class="image_accueil">
+            <img src="img/asset_accueil_map.webp" alt="image_accueil">
+        </div>
+    </section>
+
+    <section class="section_card">
+        <div class="container_card">
+            <div class="card_size">
+                <div class="container_image"></div>
+                <div class="button_reservation">Réservez maintenant</div>
+            </div>
+            <div class="card_size">
+                <div class="container_image"></div>
+                <div class="button_reservation">Réservez maintenant</div>
+            </div>
+            <div class="card_size">
+                <div class="container_image"></div>
+                <div class="button_reservation">Réservez maintenant</div>
+            </div>
+            <div class="card_size">
+                <div class="container_image"></div>
+                <div class="button_reservation">Réservez maintenant</div>
+            </div>
+            <div class="card_size">
+                <div class="container_image"></div>
+                <div class="button_reservation">Réservez maintenant</div>
+            </div>
+            <div class="card_size">
+                <div class="container_image"></div>
+                <div class="button_reservation">Réservez maintenant</div>
+            </div>
+        </div>
+
+    </section>
+
+
+
+
+</body>
